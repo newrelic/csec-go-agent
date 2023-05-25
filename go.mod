35d0983c0e46303ec37a2d5d40b0f468979c6d78
aayush-ap <agarg@newrelic.com>
2023-05-25 07:57:31 +0000

    Remove juju/fslock package dependency

--- go.mod
+++ go.mod
@@ -5,7 +5,6 @@ go 1.17
 require (
 	github.com/dlclark/regexp2 v1.9.0
 	github.com/gorilla/websocket v1.5.0
-	github.com/juju/fslock v0.0.0-20160525022230-4d5c94c67b4b
 	github.com/k2io/hookingo v1.0.2
 	github.com/mackerelio/go-osstat v0.2.4
 	github.com/pbnjay/memory v0.0.0-20210728143218-7b4eea64cf58
--- internal/security_logs/rotateFileHook.go
+++ internal/security_logs/rotateFileHook.go
@@ -11,9 +11,10 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 
-	"github.com/juju/fslock"
+	secUtils "github.com/newrelic/csec-go-agent/internal/security_utils"
 )
 
 type RotateFileConfig struct {
@@ -26,6 +27,7 @@ type RotateFileConfig struct {
 type RotateFileHook struct {
 	Config    RotateFileConfig
 	logWriter os.File
+	rotate    sync.Mutex
 }
 
 func NewRotateFileHook(config RotateFileConfig) (*RotateFileHook, *os.File, error) {
@@ -61,13 +63,40 @@ func (hook *RotateFileHook) Fire(logMessege, mode string) string {
 
 func (hook *RotateFileHook) logrollover() error {
 
-	lock := fslock.New(hook.Config.Filename)
-	lockErr := lock.TryLock()
-	if lockErr != nil {
-		//some other process has a lock on the log file so no need to rollover this
+	if !hook.rotate.TryLock() {
 		return nil
+	} else {
+		defer hook.rotate.Unlock()
 	}
 
+	lockFile := hook.Config.Filename + ".lock"
+	pid := secUtils.IntToString(os.Getpid())
+
+	if !secUtils.IsFileExist(lockFile) {
+		err := os.WriteFile(lockFile, []byte(pid), 777)
+		if err != nil {
+			return err
+		}
+	}
+	lockPid, err := os.ReadFile(lockFile)
+	if err == nil && string(lockPid) == pid {
+		err := hook.filerollover()
+		os.Remove(lockFile)
+		return err
+	} else if err == nil && string(lockPid) != pid {
+		info, err := os.Stat(lockFile)
+		if err != nil {
+			return err
+		}
+		difference := time.Now().Sub(info.ModTime())
+		if difference.Minutes() > 10 {
+			os.Remove(lockFile)
+		}
+	}
+	return err
+}
+
+func (hook *RotateFileHook) filerollover() error {
 	logFile, err := os.Open(hook.Config.Filename)
 
 	if err != nil {
@@ -91,10 +120,6 @@ func (hook *RotateFileHook) logrollover() error {
 	if err != nil {
 		return err
 	}
-	err = lock.Unlock()
-	if err != nil {
-		return err
-	}
 	return hook.DeleteFileIdNeeded(hook.Config.BaseLogFilename, filepath.Dir(hook.Config.Filename))
 
 }
@@ -110,7 +135,7 @@ func (hook *RotateFileHook) DeleteFileIdNeeded(filename, dirpath string) error {
 			dir = append(dir, file.Name())
 		}
 	}
-	if len(dir) > hook.Config.MaxBackups {
+	if len(dir) > hook.Config.MaxBackups+1 {
 		err := os.Remove(filepath.Join(dirpath, dir[0]))
 		if err != nil {
 			return err
